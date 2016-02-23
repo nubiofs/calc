12c986e4c4c2c26a024624051433912cfdecf66a
nubiofs <nubiofs@gmail.com>
2016-02-23 15:32:07 -0300

    conclusão m1c2

--- src/main/java/calc/rest/CalculatorREST.java
+++ src/main/java/calc/rest/CalculatorREST.java
@@ -1,14 +1,23 @@
 package calc.rest;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Path("calculator")
 public class CalculatorREST {
 	
+	//No Postman (para '@Consumes("application/x-www-form-urlencoded")':
+	//
+	//URL -> http://localhost:8080/calc/api/calculator/sum
+	//Body -> x-www-form-urlencoded
+	//		a=1
+	//		b=2
+	//Método -> POST
+	//
+	/*
 	@POST
 	@Path("sum")
 	@Consumes("application/x-www-form-urlencoded")
@@ -16,5 +25,82 @@ public class CalculatorREST {
 	public Integer sum(@FormParam("a") Integer a, @FormParam("b") Integer b) {
 		return a + b;
 	}
+	*/
+	
+	//No Postman (para '@Consumes("application/json")'):
+	//
+	//URL -> http://localhost:8080/calc/api/calculator/sum
+	//Body -> raw
+	//		{
+	//			"a":"1",
+	//			"b":"2"
+	//		}
+	//'Content-Type' -> JSON(application/json)
+	//Método -> POST
+	//
+	/*
+	@POST
+	@Path("sum")
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public Integer sum(Numero num) {
+		return num.a + num.b;
+	}
+	*/
 
+	//No Postman (para '@Consumes("application/xml")'):
+	//
+	//URL -> http://localhost:8080/calc/api/calculator/sum
+	//Body -> raw
+	//		<numero>
+	//			<a>1</a>
+	//			<b>2</b>
+	//		</numero>
+	//'Content-Type' -> JSON(application/xml)
+	//Método -> POST
+	//
+	/*
+	@POST
+	@Path("sum")
+	@Consumes("application/xml")
+	@Produces("text/plain")
+	public Integer sum(Numero num) {
+		return num.a + num.b;
+	}
+	*/
+
+	//No Postman (para '@Consumes({"application/json", "application/xml"})'):
+	//
+	//URL -> http://localhost:8080/calc/api/calculator/sum
+	//Body -> raw
+	//		<numero>
+	//			<a>1</a>
+	//			<b>2</b>
+	//		</numero>
+	//
+	//	"ou":
+	//
+	//		{
+	//			"a":"1",
+	//			"b":"2"
+	//		}
+	//
+	//'Content-Type' -> JSON(application/xml) "ou" JSON(application/json) 
+	//Método -> POST
+	//
+	@POST
+	@Path("sum")
+	@Consumes({"application/json", "application/xml"})
+	@Produces("text/plain")
+	public Integer sum(Numero num) {
+		return num.a + num.b;
+	}
+	
+	//Anotação para @Consumes("application/xml")
+	@XmlRootElement
+	public static class Numero {
+		public Integer a;
+		public Integer b;
+	}
+	
 }
