83db02b7046ce0945aad907df7023b9c8fda2145
nubiofs <nubiofs@gmail.com>
2016-02-23 16:52:11 -0300

    Conclusão M1C3

--- src/main/java/calc/rest/CalculatorREST.java
+++ src/main/java/calc/rest/CalculatorREST.java
@@ -1,10 +1,13 @@
 package calc.rest;
 
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import br.gov.frameworkdemoiselle.util.ValidatePayload;
 
 @Path("calculator")
 public class CalculatorREST {
@@ -92,6 +95,8 @@ public class CalculatorREST {
 	@Path("sum")
 	@Consumes({"application/json", "application/xml"})
 	@Produces("text/plain")
+	//Para a integração entre a implementação JAX-RS 1.1 com a Bean Validation.
+	@ValidatePayload
 	public Integer sum(Numero num) {
 		return num.a + num.b;
 	}
@@ -99,8 +104,34 @@ public class CalculatorREST {
 	//Anotação para @Consumes("application/xml")
 	@XmlRootElement
 	public static class Numero {
+		
+		/*
+		 * Para Body "ray" (application/json) da solicitação com:
+		 * 
+		 * {
+		 *		"a":"  ",
+		 *		"b":""
+		 *	}
+		 * 
+		 * Tem-se o Body do retorno:
+		 * 
+		 * [ 
+		 * 		{ "property": "b", "message": "não pode ser nulo" }, 
+		 * 		{ "property": "a", "message": "não pode ser nulo" } 
+		 * ]
+		 * 
+		 * Com status: 422 que representa uma "Unprocessable Entity"
+		 * (Definido pelo demoiselle para um erro provocado pela requisição do cliente 
+		 * no qual indica um problema de semântica "no caso dados nulos")
+		 * 
+		 */		
+		
+		@NotNull
 		public Integer a;
+		
+		@NotNull
 		public Integer b;
+		
 	}
 	
 }
