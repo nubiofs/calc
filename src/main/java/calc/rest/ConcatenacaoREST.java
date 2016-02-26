1edff105e79559e9b47ec9b39c14280585b7bc9c
nubiofs <nubiofs@gmail.com>
2016-02-26 09:42:02 -0300

    Conclusão do "Exercício - Módulo 1: Básico" - (Atividade Avaliativa - Módulo 1)

---
+++ src/main/java/calc/rest/ConcatenacaoREST.java
@@ -0,0 +1,59 @@
+package calc.rest;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import br.gov.frameworkdemoiselle.util.ValidatePayload;
+
+/**
+ * TESTE PARA:
+ * 
+ * "Exercício - Módulo 1: Básico" - (Atividade Avaliativa - Módulo 1)
+ * 
+ * (http://localhost:8080/calc/api/concatenacao/concat)
+ * 
+ * @author 02963357460
+ *
+ */
+@Path("concatenacao")
+public class ConcatenacaoREST {
+
+	@POST
+	@Path("concat")
+	@Consumes("application/json")
+	@Produces("application/json")
+	@ValidatePayload
+	public String concatenar(Bean bean) {
+		return bean.getTexto1() + bean.getTexto2();
+	}
+
+	public static class Bean {
+
+		@NotNull
+		private String texto1;
+
+		@NotNull
+		private String texto2;
+
+		public String getTexto1() {
+			return texto1;
+		}
+
+		public void setTexto1(String texto1) {
+			this.texto1 = texto1;
+		}
+
+		public String getTexto2() {
+			return texto2;
+		}
+
+		public void setTexto2(String texto2) {
+			this.texto2 = texto2;
+		}
+
+	}
+
+}
